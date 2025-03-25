9f0a5cbc6b64bdab5a672882569e2a1747606131
meymirele <meymisaki015@gmail.com>
2025-03-25 12:04:03 -0300

    feat: dinmanic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { CardViewComponent } from './card-view/card-view.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'card-view', component: CardViewComponent }
+  { path: 'card-view/:name', component: CardViewComponent }
 ];
--- src/app/card-view/card-view.component.ts
+++ src/app/card-view/card-view.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MoreCardsComponent } from "../more-cards/more-cards.component";
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card-view',
@@ -9,6 +10,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card-view.component.html',
   styleUrl: './card-view.component.scss'
 })
-export class CardViewComponent {
-  
+export class CardViewComponent implements OnInit {
+  name?: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.name = params['name'];
+    });
+  } //passar como propriedade
 }
